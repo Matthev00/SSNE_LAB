5246ecb978df371a5c04b400f0355840e74c0172
Matthev00 <mateuszostaszewskififa@gmail.com>
2025-03-24 10:39:22 +0100

    feat: expand hyperparameter search space in sweep configuration

--- house_val_prediction/sweep_runner.py
+++ house_val_prediction/sweep_runner.py
@@ -2,7 +2,7 @@ import wandb
 from main import main
 
 sweep_config = {
-    "method": "grid",  # Możesz zmienić na "random" dla losowego przeszukiwania
+    "method": "grid",
     "metric": {
         "name": "Validation F1",
         "goal": "maximize",
@@ -15,25 +15,25 @@ sweep_config = {
             "values": [True, False]
         },
         "HIDDEN_DIM": {
-            "value": [32, 64, 128]
+            "value": [32, 64, 128, 256, 512]
         },
         "NUM_LAYERS": {
-            "value": [1, 2, 3]
+            "value": [1, 2, 3, 4]
         },
         "DROPOUT": {
-            "value": [0.2, 0.3, 0.4, 0.5]
+            "value": [0.2, 0.3, 0.4, 0.5, 0.7]
         },
         "LR": {
-            "value": [0.001, 0.0005]
+            "value": [1e-3, 5e-4, 1e-4]
         },
         "WEIGHT_DECAY": {
-            "value": [0.0, 0.01]
+            "value": [0.0, 1e-2, 1e-3]
         },
         "BATCH_SIZE": {
             "value": [32, 64]
         },
         "epochs": {
-            "value": 10
+            "value": [10, 20]
         }
     }
 }
